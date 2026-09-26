a386a8c3ab15b92026cc6893e59e0db55bce3086
bingwaa <bingwaa@outlook.com>
2026-09-26 18:01:33 +0800

    modified:   qmdmb.user.js

--- qmdmb.user.js
+++ qmdmb.user.js
@@ -2,7 +2,7 @@
 // @name         B站直播间亲密度面板
 // @name:en      Bilibili Live Fan Medal Panel
 // @namespace    https://github.com/bingwaa/qmdmb
-// @version      1.7.3
+// @version      1.7.4
 // @author       bingwaa
 // @description     在B站直播间顶栏嵌入按钮，展示该主播粉丝团亲密度、今日获取亲密度、逐项每日任务与亲密之旅进度
 // @description:en  Enhancing the experience of watching Bilibili live streaming
@@ -30,7 +30,8 @@
   const BEAT_ID = 'qmdmb-fanpanel-beat';
   const RESYNC_TICKS = 30;
   const TOAST_ID = 'qmdmb-fanpanel-toast';
-  const ENTRY_SEL = '.follow-ctnr[data-curbutton="joinFansClub"]';
+  /* 未加入粉丝团为 unJoinFansClub，已加入为 joinFansClub */
+  const ENTRY_SEL = '.follow-ctnr[data-curbutton="joinFansClub"], .follow-ctnr[data-curbutton="unJoinFansClub"]';
   const FOLLOW_SEL = '.follow-ctnr[data-curbutton="unFollow"]';
   const OFFLINE_SEL = '.status-tag';
 
